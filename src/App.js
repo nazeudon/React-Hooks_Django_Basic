234cfcc49fb31c83265cf1618bf3c49c1b4cbc01
nazeudon <nazeudon48112@icloud.com>
2020-10-12 08:58:46 +0900

    17_Redux_conbineReducer

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useReducer } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import rootReducer from "./reducers/index";
+import { SELL_MEAT, SELL_VEGETABLE } from "./reducers/actionTypes";
+
+function App() {
+  const initialState = {
+    reducerMeat: { numOfMeat: 30 },
+    reducerVegetable: { numOfVegetable: 25 },
+  };
+
+  const [state, dispatch] = useReducer(rootReducer, initialState);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => dispatch({ type: SELL_MEAT })}>Sell Meat</button>
+        Today's Meat: {state.reducerMeat.numOfMeat}
+        <button onClick={() => dispatch({ type: SELL_VEGETABLE })}>
+          Sell Vegetable
+        </button>
+        Today's Vegetable: {state.reducerVegetable.numOfVegetable}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/reducerVegetable.js
@@ -0,0 +1,12 @@
+import { SELL_VEGETABLE } from "./actionTypes";
+
+const reducerVegetable = (state = [], action) => {
+  switch (action.type) {
+    case SELL_VEGETABLE:
+      return { ...state, numOfVegetable: state.numOfVegetable - 1 };
+    default:
+      return state;
+  }
+};
+
+export default reducerVegetable;
