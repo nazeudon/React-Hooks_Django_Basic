77331ddb5a719442f2204117aa76049f89996411
nazeudon <nazeudon48112@icloud.com>
2020-10-11 19:24:04 +0900

    11_get_api_data_id

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -3,6 +3,11 @@ import axios from "axios";
 
 const ApiFetch = () => {
   const [posts, setPosts] = useState([]);
+  const [id, setId] = useState(1);
+  const [clicked, setClicked] = useState(false);
+  const handlerClicked = () => {
+    setClicked(!clicked);
+  };
 
   useEffect(() => {
     // axiosを使うパターン
@@ -11,20 +16,31 @@ const ApiFetch = () => {
     // });
 
     // fetchを使うパターン.
-    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: "GET" })
       .then((res) => res.json()) //Html形式で返ってくるのでjsonにする
       .then((data) => {
         setPosts(data);
       });
-  }, []);
+  }, [clicked, id]);
 
   return (
     <div>
-      <ul>
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => setId(evt.target.value)}
+      ></input>
+      <br />
+      <button type="button" onClick={handlerClicked}>
+        Get Post
+      </button>
+      <br />
+      {posts.title}
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
