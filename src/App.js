8f5418ba73c435ddfdc59ac7c7562712e708adc7
nazeudon <nazeudon48112@icloud.com>
2020-10-11 21:14:55 +0900

    14_useReducer and useContext

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useReducer } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ApiFetch from "./components/ApiFetch";
+// import ApiFetch from "./components/ApiFetch";
 // import Basic2 from "./components/Basic2";
 // import BasicUseEffect from "./components/BasicUseEffect";
 // import TimerContainer from "./components/TimerContainer";
 import AppContext from "./contexts/AppContext";
-import Child from "./components/Child";
-import BasicReducer from "./components/BasicReducer";
+// import Child from "./components/Child";
+// import BasicReducer from "./components/BasicReducer";
+import CompChild from "./components/CompChild";
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "add_1":
+      return currentState + 1;
+    case "multiple_3":
+      return currentState * 3;
+    case "reset":
+      return initialState;
+    default:
+      return currentState;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <AppContext.Provider value={"value from App.js"}>
+    <AppContext.Provider
+      value={{ countProvided: count, dispatchProvided: dispatch }}
+    >
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           {/* <Basic2 /> */}
           {/* <BasicUseEffect /> */}
           {/* <TimerContainer /> */}
-          <ApiFetch />
+          {/* <ApiFetch />
           <Child />
-          <BasicReducer />
+          <BasicReducer /> */}
+          count: {count}
+          <CompChild />
         </header>
       </div>
     </AppContext.Provider>
---
+++ src/components/CompChild.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CompGrandchild from "./CompGrandchild";
+
+const CompChild = () => {
+  return (
+    <div>
+      <CompGrandchild />
+    </div>
+  );
+};
+
+export default CompChild;
