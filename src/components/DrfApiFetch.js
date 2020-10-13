4ba818307dee537bc56f27450ff778fdceb0b591
nazeudon <nazeudon48112@icloud.com>
2020-10-14 07:40:09 +0900

    25_get_method_using_id

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState([]);
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
@@ -16,6 +18,18 @@ const DrfApiFetch = () => {
       });
   }, []);
 
+  const getTask = () => {
+    axios
+      .get(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+        headers: {
+          Authorization: "Token a6b7bef4438319565d13d02f104e0f68cb8f3489",
+        },
+      })
+      .then((res) => {
+        setSelectedTask(res.data);
+      });
+  };
+
   return (
     <div>
       <ul>
@@ -25,6 +39,22 @@ const DrfApiFetch = () => {
           </li>
         ))}
       </ul>
+      Set Id
+      <br />
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => {
+          setId(evt.target.value);
+        }}
+      />
+      <br />
+      <button type="button" onClick={() => getTask()}>
+        Get task
+      </button>
+      <h3>
+        {selectedTask.title} {selectedTask.id}
+      </h3>
     </div>
   );
 };
