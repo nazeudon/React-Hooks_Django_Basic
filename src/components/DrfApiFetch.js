20441c3848b2224b00ba979bd33d7de01dfb20f9
nazeudon <nazeudon48112@icloud.com>
2020-10-14 08:38:20 +0900

    28_create_task_as_SPA

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState([]);
+  const [editedTask, setEditedTask] = useState({ id: "", title: "" });
   const [id, setId] = useState(1);
 
   useEffect(() => {
@@ -43,6 +44,28 @@ const DrfApiFetch = () => {
       });
   };
 
+  const newTask = (task) => {
+    const data = {
+      title: task.title,
+    };
+    axios
+      .post(`http://127.0.0.1:8000/api/tasks/`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token a6b7bef4438319565d13d02f104e0f68cb8f3489",
+        },
+      })
+      .then((res) => {
+        setTasks([...tasks, res.data]);
+      });
+  };
+
+  const handleInputChange = () => (evt) => {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    setEditedTask({ ...editedTask, [name]: value });
+  };
+
   return (
     <div>
       <ul>
@@ -74,6 +97,15 @@ const DrfApiFetch = () => {
       {/* <button type="button" onClick={() => deleteTask()}>
         Delete task
       </button> */}
+      <input
+        type="text"
+        name="title"
+        value={editedTask.title}
+        onChange={handleInputChange()}
+        placeholder="Nwe Task?"
+        required
+      />
+      <button onClick={() => newTask(editedTask)}>Create</button>
     </div>
   );
 };
